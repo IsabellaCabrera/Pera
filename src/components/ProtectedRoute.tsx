d68572b6071487e8331f987442480170431d7c33
abejitallorona <mariajorm2004@gmail.com>
2025-11-09 16:49:02 -0500

    update: add Loader to ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Outlet } from "react-router";
 import { useSelector } from "react-redux";
 import type { RootState } from "../redux/store";
+import { Loader } from "./Loader/Loader";
 
 interface ProtectedRouteProps {
   requiredRole?: "seller" | "customer";
@@ -10,7 +11,14 @@ export const ProtectedRoute = ({ requiredRole }: ProtectedRouteProps) => {
   const { user, isLoading } = useSelector((state: RootState) => state.auth);
 
   if (isLoading) {
-    return <div className="text-center p-8">Loading...</div>;
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center gap-5">
+          <img className="w-48" src="/peraLogo.webp" alt="Pera Logo" />
+          <Loader />
+        </div>
+      </div>
+    );
   }
 
   if (!user) {
