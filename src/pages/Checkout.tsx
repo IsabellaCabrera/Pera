ea25c9d2e7d2fe87148dc962598fe0a9612c5bd0
ItsSilva <juanse909@hotmail.com>
2025-11-09 18:04:10 -0500

    update: checkout page

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -3,37 +3,23 @@ import { ShoppingCard } from "../components/Cards/Shopping";
 import { Button } from "../components/Button";
 import { Summary } from "../components/Forms/Summary";
 import { useNavigate } from "react-router";
-
-const products = [
-  {
-    id: 1,
-    image: "/public/classicfamilybreakfast.webp",
-    title: "Classic Family Breakfast",
-    description:
-      "Wake up to a breakfast meal with a warm biscuit, fluffy scrambled eggs, savory McDonald's sausage and crispy golden Hash Browns.",
-    add: { number: 1 },
-    price: 2400,
-  },
-  {
-    id: 2,
-    image: "/public/eggburger.webp",
-    title: "Classic Egg Burger",
-    description:
-      "A delicious egg burger with a fluffy egg patty, savory sausage, and melted cheese.",
-    add: { number: 1 },
-    price: 900,
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import type { RootState } from "../redux/store";
+import { deleteFromCart } from "../redux/slices/productsSlice";
 
 export const Checkout = () => {
   const [selectedDonation, setSelectedDonation] = useState("2");
   const [selectedPayment, setSelectedPayment] = useState("card");
+  const dispatch = useDispatch();
 
-  const navigate = useNavigate()
+  const cart = useSelector((state: RootState) => state.products.cart);
 
-  const subtotal = products.reduce(
-    (total, product) => total + product.price,
-    0
+  console.log(cart);
+
+  const navigate = useNavigate();
+
+  const subtotal = Number(
+    cart.reduce((acc, offer) => acc + (offer.peraPrice || 0), 0).toFixed(2)
   );
 
   const handleDonationChange = (donation: string) => {
@@ -48,12 +34,12 @@ export const Checkout = () => {
     console.log("Processing payment...", { selectedPayment, selectedDonation });
   };
 
-  
-
   return (
     <section className="py-9 px-12">
       <div className="mb-8">
-        <Button onClick={()=> navigate(-1)} secondary>Go back</Button>
+        <Button onClick={() => navigate(-1)} secondary>
+          Go back
+        </Button>
       </div>
 
       <img src="/Pera-morado.svg" alt="Loopa Logo" className="mb-4" />
@@ -66,8 +52,12 @@ export const Checkout = () => {
           <p className="font-medium text-morado mb-8">Product detail</p>
 
           <div className="flex flex-col gap-6">
-            {products.map((product) => (
-              <ShoppingCard key={product.id} {...product} delete={() => {}} />
+            {cart.map((offer) => (
+              <ShoppingCard
+                key={offer.offerTitle}
+                offer={offer}
+                delete={() => dispatch(deleteFromCart(offer.offerTitle))}
+              />
             ))}
           </div>
         </div>
