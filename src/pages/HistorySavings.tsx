1a121ca9d5c8bb072d76ae14956195aa8207710c
abejitallorona <mariajorm2004@gmail.com>
2025-11-10 08:39:41 -0500

    update: History and saving page

--- src/pages/HistorySavings.tsx
+++ src/pages/HistorySavings.tsx
@@ -1,16 +1,23 @@
+import { useSelector } from "react-redux";
 import { NavBar } from "../components/Header/NavBar";
+import type { RootState } from "../redux/store";
+import { OrderCard } from "../components/Cards/Order";
 
 export const HistorySavings = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
   return (
     <>
       <NavBar />
       {/* Hero */}
-      <section></section>
+      <section className="hidden md:block h-[20dvh] md:h-[30dvh] xl:h-[40dvh] bg-[url(/hs.webp)] bg-center bg-cover bg-no-repeat" />
       {/* Orders */}
       <section>
-
+        {user?.orders === undefined || user.orders.length === 0 ? (
+          <p>No order yet</p>
+        ) : (
+          user?.orders.map((offer) => <OrderCard offer={offer} />)
+        )}
       </section>
-      <h1>History and Savings</h1>
     </>
   );
 };
