dc0013eab145db7490e427b37fa14846cc7d4eb1
abejitallorona <isabella.cabrera@u.icesi.edu.co>
2025-11-24 11:59:11 -0500

    feat: search for restaurant

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import { Checkbox } from "../components/Tags/Checkbox";
 import { InformativeCard } from "../components/Cards/Informative";
 import { RestaurantCard } from "../components/Cards/Restaurant";
 import { RestaurantNearYou } from "../components/Cards/RestaurantNearYou";
-import { Button } from "../components/Button";
 
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import { collection, getDocs, query, where } from "firebase/firestore";
@@ -13,6 +12,7 @@ import { db } from "../services/firebaseConfig";
 import { useDispatch, useSelector } from "react-redux";
 import { setRestaurants } from "../redux/slices/productsSlice";
 import type { RootState } from "../redux/store";
+import type { UserData } from "../types/auth";
 
 const restaurant = [
   {
@@ -58,18 +58,19 @@ const loopaInfo = [
 ];
 
 const checkboxOptions = [
-  { id: "burger", label: "Burger", img: "/burger.svg" },
-  { id: "treat", label: "Treat", img: "/treat.svg" },
+  { id: "burgers", label: "Burgers", img: "/burger.svg" },
+  { id: "sushi", label: "Sushi", img: "/rolls.svg" },
+  { id: "donuts", label: "Donuts", img: "/treat.svg" },
   { id: "hotdogs", label: "Hotdogs", img: "/hotdogs.svg" },
-  { id: "healthy", label: "Healthy", img: "/healthy.svg" },
+  { id: "bowls", label: "Bowls", img: "/healthy.svg" },
   { id: "colombian", label: "Colombian", img: "/colombian.svg" },
-  { id: "rolls", label: "Rolls", img: "/rolls.svg" },
   { id: "pizza", label: "Pizza", img: "/pizza.svg" },
   { id: "chicken", label: "Chicken", img: "/chicken.svg" },
   { id: "kebab", label: "Kebab", img: "/kebab.svg" },
 ];
 
 export const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [selected, setSelected] = useState<string[]>([]);
   const restaurants = useSelector(
     (state: RootState) => state.products.restaurants
@@ -94,7 +95,11 @@ export const Home = () => {
 
         querySnapshot.forEach((doc) => {
           const data = doc.data();
-          dispatch(setRestaurants(data));
+          const restaurant: UserData = {
+            uid: doc.id,
+            ...data,
+          };
+          dispatch(setRestaurants(restaurant));
         });
       } catch (error) {
         console.error(error);
@@ -103,30 +108,53 @@ export const Home = () => {
     getResturants();
   }, [dispatch, restaurants.length]);
 
+  const filteredRestaurants = restaurants.filter((rest) => {
+    const name = rest.name?.toLowerCase() ?? "";
+    const category = rest.category?.toLowerCase() ?? "";
+
+    const matchesName = name.includes(searchTerm.toLowerCase());
+    const matchesCategory =
+      selected.length === 0 || selected.includes(category);
+
+    return matchesName && matchesCategory;
+  });
+
   return (
     <>
       <NavBar />
       <section className="h-[440px] bg-[url(/herohomeimg.webp)] bg-center bg-cover bg-no-repeat" />
-      {/* Search Input */}
-      <section className="my-9 mx-12">
+      <section className="my-9 mx-12 flex flex-col gap-8">
+        <h2 className="font-bold text-4xl text-morado">
+          Restaurants near you!
+        </h2>
         <Input
           type="search"
           placeholder="Search for restaurants, dishes..."
           name="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-      </section>
-      {/* Checkbox */}
-      <section className="mx-12 flex justify-between flex-wrap gap-2">
-        {checkboxOptions.map(({ id, label, img }) => (
-          <Checkbox
-            key={id}
-            id={id}
-            label={label}
-            img={img}
-            checked={selected.includes(id)}
-            onChange={(isChecked) => handleChange(id, isChecked)}
-          />
-        ))}
+        <section className="mx-12 flex justify-between flex-wrap gap-2">
+          {checkboxOptions.map(({ id, label, img }) => (
+            <Checkbox
+              key={id}
+              id={id}
+              label={label}
+              img={img}
+              checked={selected.includes(id)}
+              onChange={(isChecked) => handleChange(id, isChecked)}
+            />
+          ))}
+        </section>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 mt-3">
+          {filteredRestaurants.length === 0 ? (
+            <p>No restaurants found</p>
+          ) : (
+            filteredRestaurants.map((restaurant) => (
+              <RestaurantNearYou key={restaurant.uid} restaurant={restaurant} />
+            ))
+          )}
+        </div>
       </section>
 
       {/* InformativeCard */}
@@ -143,6 +171,7 @@ export const Home = () => {
           ))}
         </section>
       </section>
+
       {/* RestaurantCard */}
       <section className="py-10 px-24 h-fit bg-Darkgray500 my-14 rounded-tl-3xl rounded-br-3xl relative ">
         <h2 className="font-bold text-xl text-white">Top picks</h2>
@@ -157,24 +186,6 @@ export const Home = () => {
           ))}
           <SlArrowLeft className="absolute top-1/2 left-4 -translate-y-1/2 text-white/30 cursor-pointer w-[32px] h-[32px]" />
           <SlArrowRight className="absolute top-1/2 right-4 -translate-y-1/2 text-white/30 cursor-pointer w-[32px] h-[32px]" />
-        </div>
-      </section>
-      {/* <RestaurantNearYouCard /> */}
-      <section className="py-9 px-12">
-        <h2 className="font-bold text-4xl text-morado pb-9">
-          Restaurants near you!
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 mt-3">
-          {restaurants.length === 0 ? (
-            <p>Loading...</p>
-          ) : (
-            restaurants.map((restaurant) => (
-              <RestaurantNearYou key={restaurant.name} restaurant={restaurant} />
-            ))
-          )}
-        </div>
-        <div className="flex justify-center mt-8">
-          <Button>See more restaurants</Button>
         </div>
       </section>
     </>
